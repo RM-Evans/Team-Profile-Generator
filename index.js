43b41c077a8c64c47031ab79ef78d70b7d3296d7
RM-Evans <evansrichardmichael@gmail.com>
2021-03-27 14:02:10 -0600

    getting the logic

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ function init() {
             type: 'checkbox',
             name: 'employeeRole',
             message: 'What is the role of the Employee?',
-            choices: ['manager', 'engineer', 'Intern']
+            choices: ['manager', 'engineer', 'Intern', "I'm done"]
+            //if 'im done' ge
         }
     ]).then(answers => {
-
-        if (answers.employeeRole === 'manager') {
+//console.log(answers.employeeRole[0])
+        if (answers.employeeRole[0] === 'manager') {
             //team.push(new Manager(answers.name, answers.email, id, officeNumber))
             inquirer.prompt([
                 {
@@ -32,12 +33,12 @@ function init() {
                     name: 'officeNumber',
                     message: 'What is your office number?'
                 }
-            ])
+            ]).then(() => console.log('hi'))
 
 
 
-        } else if (answers.employeeRole === 'engineer') {
-            team.push(new Engineer(answers.name, answers.email))
+        } else if (answers.employeeRole[0] === 'engineer') {
+            
 
             inquirer.prompt([
                 {
@@ -52,12 +53,15 @@ function init() {
                 },
                 {
                     type: 'input',
-                    name: 'officeNumber',
-                    message: 'What is your office number?'
+                    name: 'github',
+                    message: 'What is your github?'
                 }
-            ])
+            ]).then((engineerAnswers) => {
+                team.push(new Engineer(engineerAnswers.name, engineerAnswers.email, engineerAnswers.github))
+                console.log(team)
+            })
 
-        } else if (answers.employeeRole === 'Intern') {
+        } else if (answers.employeeRole[0] === 'Intern') {
             team.push(new Intern(answers.name, answers.email))
 
             inquirer.prompt([
@@ -76,13 +80,14 @@ function init() {
                     name: 'school',
                     message: 'What is your office number?'
                 }
-            ])
+            ]).then(() => console.log('hi'))
         }
 
-    }
-    }
+    })
+    
 }
 init()
+//I'm done -> are you sure done? if y, then generate html. if n, then init()
 
 
 function generateHTML(team) {
